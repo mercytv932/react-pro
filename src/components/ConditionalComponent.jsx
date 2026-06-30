f1b080e7e2a38f28d4dbcc63e5f6051070573ebc
Yosef <mercytv932@gmail.com>
2026-06-30 14:20:25 -0400

    Conditional rendering using element variables

--- src/components/ConditionalComponent.jsx
+++ src/components/ConditionalComponent.jsx
@@ -1,10 +1,16 @@
 import Welcome from "./Welcome";
 import Code from "./Code";
+
 export default function ConditionalComponent() {
-  const display = true;
+  let messageOne = <h1>This is mesage 1</h1>;
+  let messageTwo = <h1>This is mesage 2</h1>;
+  let message;
+  const display = false;
+
   if (display) {
-    return <Welcome />;
+    message = <h1>This is message one</h1>;
   } else {
-    return <Code />;
+    message = <h1>This is message 2</h1>;
   }
+  return message;
 }
