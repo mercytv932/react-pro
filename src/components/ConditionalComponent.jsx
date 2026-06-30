c5f46d6cd80618032fe01421fa14bc743636c963
Yosef <mercytv932@gmail.com>
2026-06-30 14:33:26 -0400

    ternary operator

--- src/components/ConditionalComponent.jsx
+++ src/components/ConditionalComponent.jsx
@@ -2,15 +2,7 @@ import Welcome from "./Welcome";
 import Code from "./Code";
 
 export default function ConditionalComponent() {
-  let messageOne = <h1>This is mesage 1</h1>;
-  let messageTwo = <h1>This is mesage 2</h1>;
-  let message;
-  const display = false;
+  const display = true;
 
-  if (display) {
-    message = <h1>This is message one</h1>;
-  } else {
-    message = <h1>This is message 2</h1>;
-  }
-  return message;
+  return display ? <Welcome /> : <Code />;
 }
