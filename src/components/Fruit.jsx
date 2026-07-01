1aa373c909fedab5875d6b7e6d967238b7d8242a
Yosef <mercytv932@gmail.com>
2026-07-01 14:55:14 -0400

    Conditionally Rendering List items

--- src/components/Fruit.jsx
+++ src/components/Fruit.jsx
@@ -1,7 +1,14 @@
 export default function Fruit({ name, price, emoji }) {
   return (
     <div>
-      {emoji} {name} {price}
+      {/* {emoji} {name} {price} */}
+      {price > 5 ? (
+        <li>
+          {emoji} {name} {price}
+        </li>
+      ) : (
+        ""
+      )}
     </div>
   );
 }
